d7b89dbe41cb4a8a71d0f805adaaefcd69d43b0f
Wilyereny <Wilyerenylp@gmail.com>
2023-04-13 12:13:27 -0400

    Object view was added. Dealing with object details made

--- uimodule/webapp/controller/ProductDetail.controller.js
+++ uimodule/webapp/controller/ProductDetail.controller.js
@@ -1,4 +1,10 @@
-sap.ui.define(["tutorial/products/controller/BaseController"], function(Controller) {
+sap.ui.define([
+    "tutorial/products/controller/BaseController",
+    "sap/m/MessageToast"
+  ], /**
+ * @param {typeof sap.m.MessageToast} MessageToast 
+ */
+function (Controller, MessageToast) {
     "use strict";
 
     return Controller.extend("tutorial.products.controller.ProductDetail", {
@@ -13,6 +19,9 @@ sap.ui.define(["tutorial/products/controller/BaseController"], function(Controll
             const oView = this.getView();
             oView.bindElement({
                 path: `/Products(${iProductId})`,
+                parameters: {
+                    expand: "Supplier,Category"
+                  },
                 events: {
                     dataRequested: function () {
                          oView.setBusy(true);
@@ -23,6 +32,30 @@ sap.ui.define(["tutorial/products/controller/BaseController"], function(Controll
                 }
             });
         },
+
+        addToCart: function () {
+            MessageToast.show("Added to cart");
+          },
+
+          markAsFav: function (evt) {
+            const oButton = evt.getSource();
+            if (oButton.getIcon() === "sap-icon://unfavorite") {
+              oButton.setIcon("sap-icon://favorite");
+              MessageToast.show("Added to favorites");
+              return;
+            }
+
+            oButton.setIcon("sap-icon://unfavorite");
+            MessageToast.show("Removed from favorites");
+          },
+
+          trimSuperfluousBytes: function (sVal) { // background info https://blogs.sap.com/2017/02/08/displaying-images-in-sapui5-received-from-the-northwind-odata-service/
+            if (typeof sVal === "string") {
+              const sTrimmed = sVal.substr(104);
+              return "data:image/bmp;base64," + sTrimmed;
+            }
+            return sVal;
+          },
 
      });
 });
